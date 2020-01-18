7998acb060351ce4b974a2272bb89e503629b067
kellyjeffery <kellyjeffery1@hotmail.com>
2020-01-18 16:56:24 +0000

    create a varible call parts containing and array of objects

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const Header = ({course}) => (
   </div>
 )
 
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => (
+const Content = ({parts}) => (
     <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
+      <Part part={parts[0].name} exercises={parts[0].exercises}/>
+      <Part part={parts[1].name} exercises={parts[1].exercises}/>
+      <Part part={parts[2].name} exercises={parts[2].exercises}/>
     </div>
 )
 
@@ -31,20 +31,27 @@ const Total = ({total}) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
+  
   return (
     <div>
-     <Header course={course}/>
-     <Content part1={part1} exercises1={exercises1}
-      part2={part2} exericse2={exercises2}
-      part3={part3} exercise3={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
